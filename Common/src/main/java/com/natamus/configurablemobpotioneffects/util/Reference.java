39fcc28545d9c31b9281c4498c4c16014214c556
Rick South <rick@natamus.com>
2024-05-01 16:44:04 +0200

    Configurable Mob Potion Effects source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/configurablemobpotioneffects/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.configurablemobpotioneffects.util;
+
+public class Reference {
+	public static final String MOD_ID = "configurablemobpotioneffects";
+	public static final String NAME = "Configurable Mob Potion Effects";
+	public static final String VERSION = "3.3";
+	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
+}
