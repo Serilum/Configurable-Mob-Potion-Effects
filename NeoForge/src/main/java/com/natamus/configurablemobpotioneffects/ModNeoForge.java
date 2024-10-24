1901fdfbf5a7f106eaec06677da4da1c8daa75ed
Rick South <rick@natamus.com>
2024-10-24 20:08:19 +0200

    Configurable Mob Potion Effects source code for 1.21.2.

---
+++ NeoForge/src/main/java/com/natamus/configurablemobpotioneffects/ModNeoForge.java
@@ -0,0 +1,43 @@
+package com.natamus.configurablemobpotioneffects;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.configurablemobpotioneffects.neoforge.events.NeoForgeMobEffectsEvent;
+import com.natamus.configurablemobpotioneffects.util.Reference;
+import com.natamus.configurablemobpotioneffects.util.Util;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		try {
+			Util.loadMobConfigFile();
+		} catch (Exception ex) {
+			System.out.println("[" + Reference.NAME + "] Error loading the entity config file. The mod has been disabled.");
+			return;
+		}
+
+		NeoForge.EVENT_BUS.register(NeoForgeMobEffectsEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
