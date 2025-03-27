782b2813644dfbedcb031b74e240aee4c53d4390
Rick South <rick@natamus.com>
2025-03-28 00:56:37 +0100

    Configurable Mob Potion Effects source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/configurablemobpotioneffects/forge/events/ForgeMobEffectsEvent.java
@@ -0,0 +1,27 @@
+package com.natamus.configurablemobpotioneffects.forge.events;
+
+import com.natamus.configurablemobpotioneffects.cmd.CommandCmpe;
+import com.natamus.configurablemobpotioneffects.events.MobEffectsEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeMobEffectsEvent {
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent e) {
+    	CommandCmpe.register(e.getDispatcher());
+    }
+
+	@SubscribeEvent
+	public static void onEntityJoin(EntityJoinLevelEvent e) {
+		MobEffectsEvent.onEntityJoin(e.getLevel(), e.getEntity());
+	}
+	
+	@SubscribeEvent
+	public static void onEntityDamage(LivingHurtEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		MobEffectsEvent.onEntityDamage(livingEntity.level(), livingEntity, e.getSource(), e.getAmount());
+	}
+}
