20c0674086aca9f9786dd8669cf2f9c0aa30802b
Rick South <rick@natamus.com>
2025-10-02 12:43:45 +0200

    Configurable Mob Potion Effects source code for 1.21.9.

---
+++ NeoForge/src/main/java/com/natamus/configurablemobpotioneffects/neoforge/events/NeoForgeMobEffectsEvent.java
@@ -0,0 +1,27 @@
+package com.natamus.configurablemobpotioneffects.neoforge.events;
+
+import com.natamus.configurablemobpotioneffects.cmd.CommandCmpe;
+import com.natamus.configurablemobpotioneffects.events.MobEffectsEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+
+public class NeoForgeMobEffectsEvent {
+	@SubscribeEvent
+	public static void registerCommands(RegisterCommandsEvent e) {
+		CommandCmpe.register(e.getDispatcher());
+	}
+
+	@SubscribeEvent
+	public static void onEntityJoin(EntityJoinLevelEvent e) {
+		MobEffectsEvent.onEntityJoin(e.getLevel(), e.getEntity());
+	}
+	
+	@SubscribeEvent
+	public static void onEntityDamage(LivingDamageEvent.Post e) {
+		LivingEntity livingEntity = e.getEntity();
+		MobEffectsEvent.onEntityDamage(livingEntity.level(), livingEntity, e.getSource(), e.getNewDamage());
+	}
+}
