2de4cd81b19489aa526fb25281d8c7987e4ec1e6
Rick South <rick@natamus.com>
2026-04-10 02:59:31 +0200

    Configurable Mob Potion Effects source code for 26.1.2.

---
+++ Fabric/src/main/java/com/natamus/configurablemobpotioneffects/ModFabric.java
@@ -0,0 +1,59 @@
+package com.natamus.configurablemobpotioneffects;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveEntityEvents;
+import com.natamus.configurablemobpotioneffects.cmd.CommandCmpe;
+import com.natamus.configurablemobpotioneffects.events.MobEffectsEvent;
+import com.natamus.configurablemobpotioneffects.util.Reference;
+import com.natamus.configurablemobpotioneffects.util.Util;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		try {
+			Util.loadMobConfigFile();
+		} catch (Exception ex) {
+			System.out.println("[" + Reference.NAME + "] Error loading the entity config file. The mod has been disabled.");
+			return;
+		}
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            CommandCmpe.register(dispatcher);
+        });
+
+		ServerEntityEvents.ENTITY_LOAD.register((Entity entity, ServerLevel world) -> {
+			MobEffectsEvent.onEntityJoin(world, entity);
+		});
+
+		CollectiveEntityEvents.ON_LIVING_ATTACK.register((Level world, Entity entity, DamageSource damageSource, float damageAmount) -> {
+			MobEffectsEvent.onEntityDamage(world, entity, damageSource, damageAmount);
+			return true;
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
---
+++ Forge/src/main/java/com/natamus/configurablemobpotioneffects/ModForge.java
@@ -0,0 +1,45 @@
+package com.natamus.configurablemobpotioneffects;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.configurablemobpotioneffects.forge.events.ForgeMobEffectsEvent;
+import com.natamus.configurablemobpotioneffects.util.Reference;
+import com.natamus.configurablemobpotioneffects.util.Util;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge(FMLJavaModLoadingContext modLoadingContext) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		BusGroup busGroup = modLoadingContext.getModBusGroup();
+		FMLLoadCompleteEvent.getBus(busGroup).addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		try {
+			Util.loadMobConfigFile();
+		} catch (Exception ex) {
+			System.out.println("[" + Reference.NAME + "] Error loading the entity config file. The mod has been disabled.");
+			return;
+		}
+
+    	ForgeMobEffectsEvent.registerEventsInBus();
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
