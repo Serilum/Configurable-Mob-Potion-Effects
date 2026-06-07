36cb4c8ec830701bc308e387638b8a6de2755f6d
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 19:37:03 +0200

    (v3.8) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/configurablemobpotioneffects/cmd/CommandCmpe.java
+++ Common/src/main/java/com/natamus/configurablemobpotioneffects/cmd/CommandCmpe.java
@@ -14,9 +14,9 @@ public class CommandCmpe {
 			.executes((command) -> {
 				CommandSourceStack source = command.getSource();
 				
-				MessageFunctions.sendMessage(source, "Usage:", ChatFormatting.DARK_GREEN);
+				MessageFunctions.sendTranslatableMessage(source, "collective.configurablemobpotioneffects.message.usage", ChatFormatting.DARK_GREEN);
 				MessageFunctions.sendMessage(source, " /cmpe reload", ChatFormatting.YELLOW);
-				MessageFunctions.sendMessage(source, "  Reloads the config files located in ./config/configurablemobpotioneffects/", ChatFormatting.DARK_GRAY);
+				MessageFunctions.sendTranslatableMessage(source, "  ", "collective.configurablemobpotioneffects.message.reloadsconfigfiles", ChatFormatting.DARK_GRAY);
 				return 1;
 			})
 			.then(Commands.literal("reload")
@@ -26,12 +26,12 @@ public class CommandCmpe {
 				try {
 					Util.loadMobConfigFile();
 				} catch (Exception ex) {
-					MessageFunctions.sendMessage(source, "Something went wrong while reloading the config file.", ChatFormatting.RED);
+					MessageFunctions.sendTranslatableMessage(source, "collective.configurablemobpotioneffects.message.somethingwentwrong", ChatFormatting.RED);
 					ex.printStackTrace();
 					return 0;
 				}
 				
-				MessageFunctions.sendMessage(source, "Successfully loaded the mob potion effect config files.", ChatFormatting.DARK_GREEN);
+				MessageFunctions.sendTranslatableMessage(source, "collective.configurablemobpotioneffects.message.successfullyloadedmob", ChatFormatting.DARK_GREEN);
 				return 1;
 			}))
 		);
